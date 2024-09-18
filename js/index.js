3de59e221a9516ccd74ebbee2da927ce4fd5db50
Leonardo Fuentes <blabla.13leo@gmail.com>
2024-09-18 02:18:26 -0300

    un par de correcciones ya que no podia dormir

--- js/index.js
+++ js/index.js
@@ -10,11 +10,66 @@ window.addEventListener('resize', () => {
 });
 
 /*Ahora voy a escribir el codigo para cambiar la imagen de #logo de donde esta y lo voy a insertar en el div con la clase "titulo" dentro del document */
-window.addEventListener('DOMContentLoaded', ()=>{
+
     if(window.innerWidth < 680){
 
         const titulo = document.querySelector('.titulo');
         titulo.innerHTML = "";
         titulo.innerHTML = "<div id='divLogo'></div>";
     }
-})
+
+
+ // Código para el SPA - Navegación
+ /*recorro toda la lista de elementos que poseen la clase nav-link y le aplico el evento click*/
+ document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      /*guardo en targetId el href sin el # , este va a coincidir con el id de la etiqueta */
+      const targetId = this.getAttribute('href').substring(1);
+      console.log(targetId);
+      
+      /*recorro toda la lista de elementos con la clase section y los oculto */
+      document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    
+        /*SI el id es semillas entonces muestro semillas pero no su contenido */
+      if(targetId === "semillas"){
+        document.querySelectorAll('.subcategory').forEach(section => {
+            section.style.display = 'none';
+
+          document.getElementById("semillas").style.display = 'block';
+
+            })
+
+      }else{
+        document.getElementById(targetId).style.display = 'block';
+      }
+      
+
+
+    });
+  })});
+
+  /*recorro toda la lista de elementos con la clase nav-link1 y le aplico a cada uno el evento click */
+  document.querySelectorAll('.nav-link1').forEach(submenu => {
+    submenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      console.log(targetId);
+      
+      /*oculto todas los elementos con la clase subcategory */
+      document.querySelectorAll('.subcategory').forEach(section => {
+        section.style.display = 'none';})
+      
+        /*como el id le gana a la clase, muestro el elemento que tiene un id que coincide con el href */
+      document.getElementById(targetId).style.display ='block'
+    
+    
+    
+    })}) 
+  
+      
+  // Mostrar solo la sección de inicio al cargar la página
+  document.getElementById('home').style.display = 'block';
+
